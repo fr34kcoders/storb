503c6126b2b2158157416903646ad1028319458f
shr1ftyy <syeamtechdemon@gmail.com>
2024-12-11 19:14:07 +0600

    chore: change comment

--- storage_subnet/constants.py
+++ storage_subnet/constants.py
@@ -1,10 +1,9 @@
 from enum import StrEnum
 
-
 MIN_PIECE_SIZE = 16 * 1024  # 16 KiB
 MAX_PIECE_SIZE = 16 * 1024 * 1024  # 16 MiB
 PIECE_LENGTH_SCALING = 0.5
-PIECE_LENGTH_OFFSET = 8.39  # these params should split a 1GB file to approx. 100 pieces
+PIECE_LENGTH_OFFSET = 8.39  # these params should split a 1GiB file to 128 pieces
 
 MAX_UPLOAD_SIZE = 1 * 1024 * 1024 * 1024 * 1024  # 1 TiB
 
