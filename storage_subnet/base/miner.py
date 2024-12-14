a3348794376608679f78bbf767926be9118df66c
Ray Okamoto <git@rayokamoto.com>
2024-12-14 23:50:27 +1030

    chore: format and run linting fixes

--- storage_subnet/base/miner.py
+++ storage_subnet/base/miner.py
@@ -15,12 +15,12 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-from abc import abstractmethod
 import argparse
 import asyncio
 import threading
 import time
 import traceback
+from abc import abstractmethod
 from typing import Union
 
 import bittensor as bt
