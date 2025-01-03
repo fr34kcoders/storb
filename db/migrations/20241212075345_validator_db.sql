404dd1f9bae53cfdc21b404b5bbba3345c4c47d2
rishiad <rishiadhikari5@gmail.com>
2025-01-04 00:49:03 +1030

    refactor: db schema for persistent dht

--- db/migrations/20241212075345_validator_db.sql
+++ db/migrations/20241212075345_validator_db.sql
@@ -1,30 +1,40 @@
 -- migrate:up
 
--- Table for mapping infohash to piece IDs
-CREATE TABLE infohash_chunk_ids (
-    infohash TEXT PRIMARY KEY, -- Infohash 
-    chunk_ids TEXT             -- JSON string representing chunk IDs
-);
-
--- Table for metadata associated with files
-CREATE TABLE metadata (
-    infohash TEXT PRIMARY KEY, -- Infohash 
+-- Table for mapping infohash to file metadata and chunk hashes
+CREATE TABLE tracker (
+    infohash TEXT PRIMARY KEY, -- Infohash of the file
+    validator_id INT,          -- ID of the validator
     filename TEXT,             -- Name of the file
-    timestamp TEXT,            -- Timestamp as a string (e.g., ISO 8601 format)
-    piece_length INTEGER,      -- Length of each piece in bytes
-    length INTEGER             -- Total length of the file in bytes
+    length INTEGER,            -- Length of the file in bytes
+    chunk_size INTEGER,      -- Length of each chunk in bytes
+    chunk_count INTEGER,        -- Number of chunks in the file
+    chunk_hashes TEXT,         -- JSON Array of chunk hashes
+    creation_timestamp TEXT,    -- Timestamp of creation
+    signature TEXT             -- Signature of the DHT entry by the validator
 );
 
--- -- Table for tracking infohash and validator UID in the DHT
--- CREATE TABLE tracker_dht (
---     infohash TEXT PRIMARY KEY, -- Primary key for the table
---     validator_uid TEXT         -- UID of the validator
--- );
+-- Table for mapping chunk_hash to chunk metadata and piece hashes
+CREATE TABLE chunk (
+    chunk_hash TEXT PRIMARY KEY, -- Chunk ID
+    validator_id INT,            -- ID of the validator
+    piece_hashes TEXT,           -- JSON Array of piece hashes
+    chunk_idx INTEGER,           -- Index of the chunk in the file
+    k INTEGER,                   -- Number data pieces in the chunk
+    m INTEGER,                   -- Number of total pieces in the chunk
+    chunk_size INTEGER,          -- Size of the chunk in bytes as zfec calculations
+    padlen INTEGER,              -- Padding length
+    original_chunk_size INTEGER, -- Size of the chunk in bytes before padding
+    signature TEXT               -- Signature of the DHT entry by the validator
+);
 
--- Table for mapping piece IDs to miner UIDs
-CREATE TABLE piece_id_miner_uids (
-    piece_id TEXT PRIMARY KEY, -- Primary key for the table
-    miner_uids TEXT            -- JSON string representing miner UIDs
+-- Table for mapping piece_hash to piece metadata and miner_id
+CREATE TABLE piece (
+    piece_hash TEXT PRIMARY KEY,                     -- Piece ID
+    miner_id INTEGER,                                -- ID of the miner
+    chunk_idx INTEGER,                               -- Index of the chunk in the file
+    piece_idx INTEGER,                               -- Index of the piece in the chunk
+    piece_type INTEGER CHECK (piece_type IN (0, 1)), -- Type of the piece (0: data, 1: parity)
+    signature TEXT                                   -- Signature of the DHT entry by the miner storing the piece
 );
 
 -- Table for miner stats --
@@ -44,14 +54,11 @@ CREATE TABLE miner_stats (
 -- Drop the miner_stats table
 DROP TABLE IF EXISTS miner_stats;
 
--- Drop the table mapping piece IDs to miner UIDs
-DROP TABLE IF EXISTS piece_id_miner_uids;
+-- Drop the `piece` table
+DROP TABLE IF EXISTS piece;
 
--- Drop the table for tracking infohash and validator UID in the DHT
--- DROP TABLE IF EXISTS tracker_dht;
+-- Drop the `chunk` table
+DROP TABLE IF EXISTS chunk;
 
--- Drop the table for metadata associated with files
-DROP TABLE IF EXISTS metadata;
-
--- Drop the table mapping infohash to piece IDs
-DROP TABLE IF EXISTS infohash_chunk_ids;
+-- Drop the `tracker` table
+DROP TABLE IF EXISTS tracker;
